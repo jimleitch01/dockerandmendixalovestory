19c4c0f389aa6d0154f1e810566fee64ebaa6a4a
Freek Brinkhuis <brfreek@me.com>
2017-11-06 21:33:56 +0100

    Initial commit

---
+++ server.js
@@ -0,0 +1,64 @@
+'use-strict';
+
+const express = require('express');
+const loki = require('lokijs');
+const bodyParser = require('body-parser');
+const uuidv1 = require('uuid/v1');
+
+const db = new loki('loki.json');
+const puppies = db.addCollection('puppies');
+const app = express();
+
+app.use(bodyParser.json());
+app.get('/', (req, res) => {
+    res.send("Thanks");
+});
+
+app.post('/pup', (req, res) => {
+    console.log(JSON.stringify(req.body));
+    var pup = req.body;
+    
+    var existingPupperito = puppies.find({name: pup.name});
+    if(existingPupperito.length > 0){
+        res.status(409);
+        
+        res.send(JSON.stringify({error: 'A pupperito with this name already exists, please give him a new one'}));
+        
+    } else {
+        
+        pup.uuid = uuidv1();
+        console.log(JSON.stringify(pup));
+        puppies.insert(pup);
+        res.send(JSON.stringify(pup));
+    }   
+});
+
+app.put('/pup/:uuid', (req, res) => {
+    const uuid = req.params.uuid;
+    var pupper = puppies.find({'uuid': uuid});
+    var updatePupper = req.body;
+    if(pupper){
+        pupper.name = updatePupper.Name;
+        pupper.breed = updatePupper.Breed;
+        pupper.age = updatePupper.Age;
+        puppies.update(pupper);
+    }
+
+});
+app.get('/pup', (req, res) => {
+    const puppers = puppies.chain().simplesort("name").data();
+
+    res.send(puppers);
+});
+
+app.get('/pup/:uuid', (req, res) => {
+    const pupper = puppies.find({uuid: req.params.uuid});
+    if(pupper.length > 0){
+        res.send(JSON.stringify(pupper[0]));
+    } else {
+        res.status(404);
+        res.send(JSON.stringify({error: 'pupper not found'}));
+    }
+});
+
+app.listen(3005);

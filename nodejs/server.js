e15c8dfdfdc3c96fce1e99f16fdc38e764360efd
Jim Leitch <jim.leitch@oelan.nl>
2017-11-20 21:22:17 +0100

    updating all the stufffz

--- nodejs/server.js
+++ nodejs/server.js
@@ -4,15 +4,17 @@ const express = require('express');
 const loki = require('lokijs');
 const bodyParser = require('body-parser');
 const uuidv1 = require('uuid/v1');
-
-const db = new loki('loki.json');
-const puppies = db.addCollection('puppies');
+const db = new loki('./loki.json');
 const app = express();
+const puppies = db.addCollection('puppies');        
+
+app.use(bodyParser.json());
+
 
 const initData = [
     {
         name: 'Pupperito',
-        breed: 'Cotton Terrier',
+        breed: 'Cotton_Terrier',
         age: 0.4,
         uuid: uuidv1()
     },
@@ -25,7 +27,7 @@ const initData = [
     ,
     {
         name: 'Balou',
-        breed: 'Golden Brinback',
+        breed: 'Golden_Bringback',
         age: 10,
         uuid: uuidv1()
     }
@@ -33,9 +35,43 @@ const initData = [
 
 puppies.insert(initData);
 
-app.use(bodyParser.json());
-app.get('/', (req, res) => {
-    res.send("Thanks");
+app.get('/pup', (req, res) => {
+    console.log(db);
+    if(puppies){
+        const puppers = puppies.chain().simplesort("name").data();
+        res.send(puppers);
+    } else {
+        res.status(404);
+        res.send(JSON.stringify({error: '404, puppers not found'}));
+    }
+    
+});
+
+app.get('/pup/:uuid', (req, res) => {
+    const pupper = puppies.find({uuid: req.params.uuid});
+    if(pupper.length > 0){
+        res.send(JSON.stringify(pupper[0]));
+    } else {
+        res.status(404);
+        res.send(JSON.stringify({error: 'pupper not found'}));
+    }
+});
+
+app.put('/pup/:uuid', (req, res)=>{
+    const uuid = req.params.uuid;
+    var pupper = puppies.find({'uuid': uuid});
+    var updatePupper = req.body;
+    if(pupper){
+        pupper.name = updatePupper.Name;
+        pupper.breed = updatePupper.Breed;
+        pupper.age = updatePupper.Age;
+        puppies.update(pupper);
+        res.status(200);
+        res.send(JSON.stringify({success: 'true'}));
+    } else {
+        res.status(500);
+        res.send(JSON.stringify({error: 'woof'}));
+    }
 });
 
 app.post('/pup', (req, res) => {
@@ -57,32 +93,4 @@ app.post('/pup', (req, res) => {
     }   
 });
 
-app.put('/pup/:uuid', (req, res) => {
-    const uuid = req.params.uuid;
-    var pupper = puppies.find({'uuid': uuid});
-    var updatePupper = req.body;
-    if(pupper){
-        pupper.name = updatePupper.Name;
-        pupper.breed = updatePupper.Breed;
-        pupper.age = updatePupper.Age;
-        puppies.update(pupper);
-    }
-
-});
-app.get('/pup', (req, res) => {
-    const puppers = puppies.chain().simplesort("name").data();
-
-    res.send(puppers);
-});
-
-app.get('/pup/:uuid', (req, res) => {
-    const pupper = puppies.find({uuid: req.params.uuid});
-    if(pupper.length > 0){
-        res.send(JSON.stringify(pupper[0]));
-    } else {
-        res.status(404);
-        res.send(JSON.stringify({error: 'pupper not found'}));
-    }
-});
-
-app.listen(3005);
+app.listen(3000);
